de50aee9e20737260eda979c681c0532e878d254
kikemx78 <edelbosque@e-sant.com>
2022-11-23 22:05:29 -0600

    first commit

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,20 +1,25 @@
 import type { GatsbyConfig } from "gatsby";
 
-const config: GatsbyConfig = {
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}` // or '.env'
+});
+// // And then you can use the config in gatsby-config.js
+const config = require('gatsby-plugin-config').default;
+
+const gatsbyConfig: GatsbyConfig = {
+  pathPrefix:
+  process.env.NODE_ENV === 'development' ? `` : `/cws/new-ui`,
   siteMetadata: {
-    title: `ansible-test-ui`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `Ansible test`,
+    description: ``,
+    author: `---`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-styled-components", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }]
+  plugins: ["gatsby-plugin-styled-components"]
 };
 
-export default config;
+module.exports = gatsbyConfig;
+
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,44 +1,15 @@
 import * as React from "react"
 import { Link, HeadFC, PageProps } from "gatsby"
 
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-const NotFoundPage: React.FC<PageProps> = () => {
+const NotFoundPage: React.FC<PageProps> = ({ location }) => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
+    <main>
+      <h1>Page not found</h1>
+      <p>
         Sorry 😔, we couldn’t find what you were looking for.
         <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
+        {JSON.stringify(location)}
+        <Link to="/?newui=12345">Go home</Link>.
       </p>
     </main>
   )
